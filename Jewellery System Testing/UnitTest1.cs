6939a5755d505825d5e07d96d03ce7527b7e078e
StephaneHarvey <stephaneharvey9@gmail.com>
2022-02-08 16:13:25 +0000

    Add project files.

---
+++ Jewellery System Testing/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Jewellery_System_Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
