a5cac71f0b97e4ed3938dae29608dc1b0aea1773
StephaneHarvey <stephaneharvey9@gmail.com>
2022-02-09 12:10:27 +0000

    Added testing for order class

---
+++ Jewellery System Testing/tstOrder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Jewellery_System_Testing
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsOrder AnOrder = new clsOrder();
+            //test to see that is exists 
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+
+    class clsOrder
+    {
+    }
+}
