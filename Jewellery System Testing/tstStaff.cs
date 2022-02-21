7944596525d96a1d6208a38bf97270d5e6572973
ShabanaHamir <P2618936@my365.dmu.ac.uk>
2022-02-21 12:54:51 +0000

    Added testing for staff class

---
+++ Jewellery System Testing/tstStaff.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Jewellery_System_Testing
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test to see that it exists
+            Assert.IsNotNull(AStaff);
+        }
+    }
+
+    class clsStaff
+    {
+
+    }
+}
