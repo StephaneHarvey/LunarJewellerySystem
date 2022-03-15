1eaccd7fa60212fed59d03c31d64cef0ecf87219
ShabanaHamir <P2618936@my365.dmu.ac.uk>
2022-03-15 19:24:15 +0000

    clsStaff updated

--- Jewellery System Testing/tstStaff.cs
+++ Jewellery System Testing/tstStaff.cs
@@ -22,7 +22,7 @@ namespace Jewellery_System_Testing
             //create an instance of the class that we want to create
             clsStaff AStaff = new clsStaff();
             //create some test data to assign to property
-            string TestData = "67 Cross Close";           
+            string TestData = "67 Cross Close";
             //assign the data to the property
             AStaff.StaffAddress = TestData;
             //test to see that it exists
@@ -42,18 +42,18 @@ namespace Jewellery_System_Testing
             Assert.AreEqual(AStaff.StaffContactNo, TestData);
         }
 
-        //[TestMethod]
-        //public void StaffDOBPropertyOK()
-        //{
-        //    //create an instance of the class 
-        //    clsStaff AStaff = new clsStaff();
-        //    //create test data to assign to property
-        //    DateTime TestData = "07763890134";
-        //    //assign the data to the property
-        //    AStaff.StaffDOB = TestData;
-        //    //test to see that it exists
-        //    Assert.AreEqual(AStaff.StaffContactNo, TestData);
-        //}
+        [TestMethod]
+        public void StaffDOBPropertyOK()
+        {
+            //create an instance of the class 
+            clsStaff AStaff = new clsStaff();
+            //create test data to assign to property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStaff.StaffDOB = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AStaff.StaffDOB, TestData);
+        }
 
         [TestMethod]
         public void StaffFirstNamePropertyOK()
@@ -106,11 +106,121 @@ namespace Jewellery_System_Testing
             //boolean variable to record if data is OK 
             Boolean OK = true;
             //test data to use with the method
-            Int32 StaffID = 2618936;
+            Int32 StaffID = 21;
             //invoke the method
             Found = AStaff.Find(StaffID);
             //check staffID 
-            if (AStaff.StaffID != 2618936)
+            if (AStaff.StaffID != 21)
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffFirstNameFound()
+        {
+            //create instance of class
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the results of the validation 
+            Boolean Found = false;
+            //boolean variable to record if data is OK 
+            Boolean OK = true;
+            //test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check staffFirstName 
+            if (AStaff.StaffFirstName != "Gwenyth")
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffLastNameFound()
+        {
+            //create instance of class
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the results of the validation 
+            Boolean Found = false;
+            //boolean variable to record if data is OK 
+            Boolean OK = true;
+            //test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check stafflasttName 
+            if (AStaff.StaffLastName != "Paltrow")
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffAddressFound()
+        {
+            //create an instance of the class
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the results of the validation 
+            Boolean Found = false;
+            //boolean variable to record if data is OK 
+            Boolean OK = true;
+            //test data to use
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check address 
+            if (AStaff.StaffAddress != "67 Cross Close")
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffContactNoFound()
+        {
+            //create an instance of the class
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the results of the validation 
+            Boolean Found = false;
+            //boolean variable to record if data is OK 
+            Boolean OK = true;
+            //test data to use
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check contactno 
+            if (AStaff.StaffContactNo != "07763890134")
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffDOBFound()
+        {
+            //create an instance of the class
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the results of the validation 
+            Boolean Found = false;
+            //boolean variable to record if data is OK 
+            Boolean OK = true;
+            //test data to use
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check address 
+            if (AStaff.StaffDOB != Convert.ToDateTime("10/10/1999"))
             {
                 OK = false;
             }
@@ -118,6 +228,4 @@ namespace Jewellery_System_Testing
             Assert.IsTrue(OK);
         }
     }
-
-   
 }
